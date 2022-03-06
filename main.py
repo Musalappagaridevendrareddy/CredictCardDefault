b2a2473c0c09e0a8f2fa06ac765ce1501e8adc67
DevendarDev <66067336+Musalappagaridevendrareddy@users.noreply.github.com>
2022-03-06 20:18:17 +0530

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_file
 from flask import Response
 import os
 from flask_cors import CORS, cross_origin
@@ -14,85 +14,94 @@ os.putenv('LC_ALL', 'en_US.UTF-8')
 app = Flask(__name__)
 # dashboard.bind(app)
 CORS(app)
+UPLOAD_FOLDER = 'Prediction_Output_File/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route("/", methods=['GET'])
 @cross_origin()
 def home():
-	return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route("/predict", methods=['GET', 'POST'])
 @cross_origin()
 def predictRouteClient():
-	try:
-		if request.method == 'POST' and request.form.get('action1') == 'Custom File Predict':
-			f = request.files['csvfile']
-			f.save('upload_data/' + f.filename)
-			path = 'upload_data'
-			pred_val = pred_validation(path)  # object initialization
+    try:
+        if request.method == 'POST' and request.form.get('action1') == 'Custom File Predict':
+            f = request.files['csvfile']
+            f.save('upload_data/' + f.filename)
+            path = 'upload_data'
+            pred_val = pred_validation(path)  # object initialization
 
-			pred_val.prediction_validation()  # calling the prediction_validation function
+            pred_val.prediction_validation()  # calling the prediction_validation function
 
-			pred = prediction(path)  # object initialization
+            pred = prediction(path)  # object initialization
 
-			# predicting for dataset present in database
-			path = pred.predictionFromModel()
-			return render_template('index.html', path="Prediction File created at %s!!!" % path)
-		elif request.method == 'POST' and request.form.get('action2') == 'Default File Predict':
-			path = 'Prediction_Batch_files'
-			pred_val = pred_validation(path)  # object initialization
+            # predicting for dataset present in database
+            path = pred.predictionFromModel()
+            return render_template('index.html', path="Prediction File created at %s!!!" % path, filename='Predictions.csv')
+        elif request.method == 'POST' and request.form.get('action2') == 'Default File Predict':
+            path = 'Prediction_Batch_files'
+            pred_val = pred_validation(path)  # object initialization
 
-			pred_val.prediction_validation()  # calling the prediction_validation function
+            pred_val.prediction_validation()  # calling the prediction_validation function
 
-			pred = prediction(path)  # object initialization
+            pred = prediction(path)  # object initialization
 
-			# predicting for dataset present in database
-			path = pred.predictionFromModel()
-			return render_template('index.html', path="Prediction File created at %s!!!" % path)
+            # predicting for dataset present in database
+            path = pred.predictionFromModel()
+            return render_template('index.html', path="Prediction File created at %s!!!" % path, filename='Predictions.csv')
 
-	except ValueError:
+    except ValueError:
 
-		return Response("Error Occurred! %s" % ValueError)
+        return Response("Error Occurred! %s" % ValueError)
 
-	except KeyError:
+    except KeyError:
 
-		return Response("Error Occurred! %s" % KeyError)
+        return Response("Error Occurred! %s" % KeyError)
 
-	except Exception as e:
+    except Exception as e:
 
-		return Response("Error Occurred! %s" % e)
-	return Response("Training successfull!!")
+        return Response("Error Occurred! %s" % e)
+    return Response("Training successfull!!")
 
 
 @app.route("/train", methods=['POST'])
 @cross_origin()
 def trainRouteClient():
-	try:
-		if request.json['filepath'] is not None:
-			path = request.json['filepath']
-			train_valObj = train_validation(path)  # object initialization
+    try:
+        if request.json['filepath'] is not None:
+            path = request.json['filepath']
+            train_valObj = train_validation(path)  # object initialization
 
-			train_valObj.train_validation()  # calling the training_validation function
+            train_valObj.train_validation()  # calling the training_validation function
 
-			trainModelObj = trainModel()  # object initialization
-			trainModelObj.trainingModel()  # training the model for the files in the table
+            trainModelObj = trainModel()  # object initialization
+            trainModelObj.trainingModel()  # training the model for the files in the table
 
 
-	except ValueError:
+    except ValueError:
 
-		return Response("Error Occurred! %s" % ValueError)
+        return Response("Error Occurred! %s" % ValueError)
 
-	except KeyError:
+    except KeyError:
 
-		return Response("Error Occurred! %s" % KeyError)
+        return Response("Error Occurred! %s" % KeyError)
 
-	except Exception as e:
+    except Exception as e:
 
-		return Response("Error Occurred! %s" % e)
-	return Response("Training successfull!!")
+        return Response("Error Occurred! %s" % e)
+    return Response("Training successfull!!")
+
+
+@app.route('/return-files/<filename>')
+def return_files_tut(filename):
+    file_path = UPLOAD_FOLDER + filename
+    return send_file(file_path, as_attachment=True, attachment_filename='')
 
 
 if __name__ == "__main__":
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
